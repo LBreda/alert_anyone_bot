fe33e20bb47e39b03bc16eac9a5c2e53df7e3460
Lorenzo Breda <lorenzo@lbreda.com>
2021-02-26 00:37:16 +0100

    :sparkles: Uses the id instead of the username

--- main.js
+++ main.js
@@ -7,39 +7,52 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 let dir = ctx => {
     let roomId = ctx.message.chat.id
     let dirName = `data/${roomId}`
-    if(!fs.existsSync(dirName)) fs.mkdirSync(`data/${roomId}`);
+    if (!fs.existsSync(dirName)) fs.mkdirSync(`data/${roomId}`);
     return dirName
+}
+
+let userData = ctx => {
+    let sender = ctx.message.from
+    return {
+        id: sender.id,
+        handle: sender.first_name || sender.username || sender.id
+    }
 }
 
 bot.command('all', ctx => {
     let room_id = ctx.message.chat.id
     let directory = dir(ctx)
-    let users = fs.readdirSync(directory).filter(file => !file.startsWith('.')).map(user => `@${user}`)
-    ctx.reply(`Hey, ${users.join(' ')}`)
+    let users = fs.readdirSync(directory)
+        .filter(file => !file.startsWith('.'))
+        .map(file => {
+            let userData = JSON.parse(fs.readFileSync(`${directory}/${file}`).toString())
+            return `[${userData.handle}](tg://user?id=${userData.id})`
+        })
+    ctx.replyWithMarkdown(`Hey, ${users.join(' ')}`)
 })
 
-bot.command('alertyme', ctx => {
-    let userId = ctx.message.from.id
+bot.command('alertme', ctx => {
+    let user = userData(ctx)
     let directory = dir(ctx)
-    fs.writeFile(`${directory}/${username}`, '', e => {
-        if(e) {
-            ctx.reply(`I'm Ill and I couldn't save your preferences, [${userId}](tg://user?id=${userId})! 🤒`)
+    fs.writeFile(`${directory}/${user.id}`, JSON.stringify(user), e => {
+        if (e) {
+            ctx.reply(`I'm Ill and I couldn't save your preferences, [${user.handle}](tg://user?id=${user.id})! 🤒`)
             console.log(e)
         } else {
-            ctx.reply(`I'll notify you, [${userId}](tg://user?id=${userId})! 👍`)
+            ctx.reply(`I'll notify you, [${user.handle}](tg://user?id=${user.id})! 👍`)
         }
     })
 })
 
 bot.command('dontalertme', ctx => {
-    let userId = ctx.message.from.id
+    let user = userData(ctx)
     let directory = dir(ctx)
-    fs.unlink(`${directory}/${userId}`, e => { 
-        if(e) {
-            ctx.reply(`I'm Ill and I couldn't save your preferences, [${userId}](tg://user?id=${userId})! 🤒`)
+    fs.unlink(`${directory}/${user.id}`, e => {
+        if (e) {
+            ctx.reply(`I'm Ill and I couldn't save your preferences, [${user.handle}](tg://user?id=${user.id})! 🤒`)
             console.log(e)
         } else {
-            ctx.reply(`I won't notify you anymore, [${userId}](tg://user?id=${userId})! 👍`)
+            ctx.reply(`I won't notify you anymore, [${user.handle}](tg://user?id=${user.id})! 👍`)
         }
     })
 })
@@ -47,7 +60,7 @@ bot.command('dontalertme', ctx => {
 bot.on('left_chat_member', ctx => {
     let userId = ctx.message.from.id
     let directory = dir(ctx)
-    if(fs.existsSync(`${directory}/${userId}`)){
+    if (fs.existsSync(`${directory}/${userId}`)) {
         fs.unlinkSync(`${directory}/${userId}`)
     }
 })
